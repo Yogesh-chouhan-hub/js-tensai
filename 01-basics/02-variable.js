25d27716e6b69ad14ec01ab7a12f7cd19262e376
Yogesh-chouhan-hub <yogeshchouhan2253@gmail.com>
2025-05-12 18:57:45 +0000

    learned datatype,variable

---
+++ 01-basics/02-variable.js
@@ -0,0 +1,18 @@
+const account_id = 144553;  //assigned value cant be changed
+let account_email = "yogesh@123.gmail.com";
+var account_password = "112345";
+accountCity = "indore";
+let account_state;
+
+// account_id = 2;
+// not allowed 
+/*
+please dont use var variable in js
+*/
+
+account_email = "renu@123"
+account_password = "2345678i"
+accountCity = "jaipur"
+
+console.log(account_id);
+console.table([account_email,account_id,account_password,accountCity,account_state])
---
+++ 01-basics/03-datatype.js
@@ -0,0 +1,25 @@
+let name ="yogesh chouhan "
+let age = 21 
+let isloggeddin = true
+
+
+console.table([name,age,isloggeddin])
+//number 
+//Bigint
+//strings
+//boolean
+//null => stand alone value 
+//undefine => declare but not defined
+//symbol => to find the uniqueness
+
+//object 
+//array
+
+
+console.log(typeof "hitesh");
+console.log(typeof "age");
+console.log(typeof "isloggedin");
+console.log(typeof null);
+
+
+//type of null is object 
