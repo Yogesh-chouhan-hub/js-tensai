1c4a68d103cb8c2656f237c9bce1a92e62d768e2
Yogesh-chouhan-hub <yogeshchouhan2253@gmail.com>
2025-05-12 19:22:12 +0000

    conversion of da tatype

---
+++ 01-basics/04-conversion.js
@@ -0,0 +1,22 @@
+// let score =33;
+// console.log(typeof score);
+
+// let score ="33abc";
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber);    //NaN
+
+
+//"33" => 33
+//"33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedin = 1 
+let booleanIsLoggedIn = Boolean(isLoggedin);
+console.log(booleanIsLoggedIn);
+
+
+//"" => false 
+// "hitesh"=> true
