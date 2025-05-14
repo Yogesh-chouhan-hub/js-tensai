ae13ddf649ae40529472f70d44fcde32ae7c9e4e
Yogesh-chouhan-hub <yogeshchouhan2253@gmail.com>
2025-05-14 21:39:15 +0000

    hello

---
+++ 07-string.js
@@ -0,0 +1,14 @@
+const str1 = "yogesh-chouhan"
+console.log(str1.slice(-10,4))
+
+const  str2 = "    yogesh chouhan     "
+console.log(str2);
+console.log(str2.trim());
+
+
+const str3 = "http://hitesh.com/yogesh@gmail.com";
+str3.replace('yogesh','hitesh')
+console.log(str3.replace('yogesh','hitesh'))
+
+//http://hitesh.com/hitesh@gmail.com
+
